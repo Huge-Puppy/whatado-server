b3b077bd1fd2080adcbce4242407dea2b89fa02d
jared lambert <jaredclambert@gmail.com>
2022-08-12 22:36:45 +0000

    prelaunch prod 8/12

--- src/migrations/1639682204419-addfriends.ts
+++ src/migrations/1639682204419-addfriends.ts
@@ -1,13 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class addfriends1639682204419 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      'CREATE TABLE "user_friends_user" ("userId_1" integer NOT NULL, "userId_2" integer NOT NULL, CONSTRAINT "PK_f2b5631d91f6b7fda632135932f" PRIMARY KEY ("userId_1", "userId_2"))'
-    );
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query('DROP TABLE "user_friends_user');
-  }
-}
+//import { MigrationInterface, QueryRunner } from "typeorm";
+//
+//export class addfriends1639682204419 implements MigrationInterface {
+//  public async up(queryRunner: QueryRunner): Promise<void> {
+//    await queryRunner.query(
+//      'CREATE TABLE "user_friends_user" ("userId_1" integer NOT NULL, "userId_2" integer NOT NULL, CONSTRAINT "PK_f2b5631d91f6b7fda632135932f" PRIMARY KEY ("userId_1", "userId_2"))'
+//    );
+//  }
+//
+//  public async down(queryRunner: QueryRunner): Promise<void> {
+//    await queryRunner.query('DROP TABLE "user_friends_user');
+//  }
+//}
--- src/migrations/1639848978790-changeAgeFilter.ts
+++ src/migrations/1639848978790-changeAgeFilter.ts
@@ -1,20 +1,20 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-
-export class changeAgeFilter1639848978790 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      'ALTER TABLE "event" DROP COLUMN "filterAge", ADD COLUMN "filterMinAge" INT, ADD COLUMN "filterMaxAge" INT;'
-    );
-    await queryRunner.query('UPDATE "event" SET "filterMinAge" = 18;');
-    await queryRunner.query('UPDATE "event" SET "filterMaxAge" = 70;');
-    await queryRunner.query(
-      'ALTER TABLE "event" ALTER COLUMN "filterMaxAge" SET NOT NULL, ALTER COLUMN "filterMinAge" SET NOT NULL;'
-    );
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(
-      'ALTER TABLE "event" DROP COLUMN "filterMinAge", DROP COLUMN "filterMaxAge", ADD COLUMN "filterAge" VARCHAR NOT NULL;'
-    );
-  }
-}
+//import { MigrationInterface, QueryRunner } from "typeorm";
+//
+//export class changeAgeFilter1639848978790 implements MigrationInterface {
+//  public async up(queryRunner: QueryRunner): Promise<void> {
+//    await queryRunner.query(
+//      'ALTER TABLE "event" DROP COLUMN "filterAge", ADD COLUMN "filterMinAge" INT, ADD COLUMN "filterMaxAge" INT;'
+//    );
+//    await queryRunner.query('UPDATE "event" SET "filterMinAge" = 18;');
+//    await queryRunner.query('UPDATE "event" SET "filterMaxAge" = 70;');
+//    await queryRunner.query(
+//      'ALTER TABLE "event" ALTER COLUMN "filterMaxAge" SET NOT NULL, ALTER COLUMN "filterMinAge" SET NOT NULL;'
+//    );
+//  }
+//
+//  public async down(queryRunner: QueryRunner): Promise<void> {
+//    await queryRunner.query(
+//      'ALTER TABLE "event" DROP COLUMN "filterMinAge", DROP COLUMN "filterMaxAge", ADD COLUMN "filterAge" VARCHAR NOT NULL;'
+//    );
+//  }
+//}
